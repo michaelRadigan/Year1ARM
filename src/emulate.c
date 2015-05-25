6eb0e271fbb49a9f126cefed6da2c71a08eeda59
Michael Radigan <mr3914@ic.ac.uk>
2015-05-25 20:51:43 +0100

    added funtion to initialise the cpustruct

--- src/emulate.c
+++ src/emulate.c
@@ -1,12 +1,28 @@
 #include "common_instances.h"
 
 typedef struct cpu{
-  uint32_t *register[14] 
+  uint32_t *reg[14]; 
   uint32_t *pc;
   uint32_t *cpsr;
   uint32_t decode;
   uint32_t encode;
 }cpu;
+
+//this will execute the next fetch
+uint32_t nextFetch();
+
+
+void setUpCycle(){
+    cpu cpustruct;
+    cpustruct.pc = 0;
+    cpustruct.cpsr = 0;
+    int i;
+    for(i = 0; i< 14; i++){
+        cpustruct.reg[i] = 0;
+    }
+    cpustruct.decode = nextFetch();
+    cpustruct.encode = nextFetch();
+}
 
 
 
