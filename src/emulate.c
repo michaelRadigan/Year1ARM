f61556149fdd693e562d96da776588a0a6ba53ea
hl6114 <hl6114@ic.ac.uk>
2015-05-25 20:02:40 +0100

    cpu struct added to emulate.c

--- src/emulate.c
+++ src/emulate.c
@@ -1,4 +1,14 @@
-#include <stdlib.h>
+#include "common_instances.h"
+
+typedef struct cpu{
+  uint32_t *register[14] 
+  uint32_t *pc;
+  uint32_t *cpsr;
+  uint32_t decode;
+  uint32_t encode;
+}cpu;
+
+
 
 
 
