713de74afc29d73f4713b2dda3f8d69f9b906ea1
hh2214 <hh2214@ic.ac.uk>
2015-05-29 16:33:02 +0100

    Sorry Michael i'll get you a beer :(

---
+++ Debug/src/stack.d
@@ -0,0 +1,12 @@
+src/stack.d: ../src/stack.c ../src/stack.h ../src/common_instances.h \
+ ../src/cpu.h ../src/memory_proc.h ../src/instructions.h
+
+../src/stack.h:
+
+../src/common_instances.h:
+
+../src/cpu.h:
+
+../src/memory_proc.h:
+
+../src/instructions.h:
