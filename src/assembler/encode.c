2e59255986940a9a872ead9dc4c65fbd98d6d500
hl6114 <hl6114@ic.ac.uk>
2015-05-29 14:48:36 +0100

    All Parts of assembler should in theory be working, ready to be filled in

--- src/assembler/encode.c
+++ src/assembler/encode.c
@@ -1,6 +1,58 @@
 #include "encode.h"
 
 
+
+/* 
+  Sets up structs containing function pointers 
+  This is a stupid method but cannot think of
+  anything better at the moment 
+*/
+void setUPFuncStructs(void){
+  STR_ENC *dp1 = malloc(sizeof(STR_ENC));
+  dp1->encFunc = &dataProcessing1;
+  
+  STR_ENC *dp2 = malloc(sizeof(STR_ENC));
+  dp2->encFunc = &dataProcessing2;
+
+  STR_ENC *dp3 = malloc(sizeof(STR_ENC));
+  dp3->encFunc = &dataProcessing3;
+
+  STR_ENC *m = malloc(sizeof(STR_ENC));
+  m->encFunc = &multiply;
+
+  STR_ENC *ma = malloc(sizeof(STR_ENC));
+  ma->encFunc = &multiplyAccum;
+
+  STR_ENC *ldr = malloc(sizeof(STR_ENC));
+  ldr->encFunc = &sdt_ldr;
+
+  STR_ENC *str = malloc(sizeof(STR_ENC));
+  str->encFunc = &sdt_str;
+
+  STR_ENC *b = malloc(sizeof(STR_ENC));
+  b->encFunc = &branch;
+
+  STR_ENC *andeq = malloc(sizeof(STR_ENC));
+  andeq->encFunc = &spec_andeq;
+
+  STR_ENC *lsl = malloc(sizeof(STR_ENC));
+  lsl->encFunc = &spec_lsl;
+
+}
+
+void destroyFuncStructs(void){
+  free(dp1);
+  free(dp2);
+  free(dp3);
+  free(m);
+  free(ma);
+  free(ldr);
+  free(str);
+  free(b);
+  free(andeq);
+  free(lsl);
+}
+
 /* char *source is the string of words of the instructions */
 
 /* Translates and, eor, sub, rsb, add ,orr instructions*/
