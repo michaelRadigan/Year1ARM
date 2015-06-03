d273eb8861285263a4772ebe8dfba78c595f593e
hh2214 <hh2214@ic.ac.uk>
2015-06-03 15:44:07 +0100

    Finished cleaning code -> need to look at deaclocation and removed stack files

--- src/memory_proc.c
+++ src/memory_proc.c
@@ -1,18 +1,27 @@
 #include "common_instances.h"
 
 
-#define BYTES    1
+/* Pointer declarations for structs*/
 
 
 memory_machine *memory;
 pipeline_obj *pipeline_ptr;
 
+instr_data_proc_struct *instr_data_proc_ptr;
+instr_mult_struct *instr_mult_ptr;
+instr_single_data_trans_struct *instr_single_data_trans_ptr;
+instr_branch_struct *instr_branch_ptr;
 
+/**
+ * Converts a word from Big Endian to Little Endian
+ * @param word The word to be converted
+ */
 uint32_t
 memory_swap_word_be_to_le(uint32_t word){
-	word = ((word << 8) & 0xFF00FF00) | ((word >> 8) & 0x00FF00FF);
-	return (word << 16) | (word >> 16);
+  word = ((word << 8) & 0xFF00FF00) | ((word >> 8) & 0x00FF00FF);
+  return (word << 16) | (word >> 16);
 }
+
 
 /**
  * Fetches 4 bytes from memory and concatenates into 1 word
@@ -21,39 +30,32 @@ memory_swap_word_be_to_le(uint32_t word){
 uint32_t
 memory_fetch_word(uint32_t pc){
 
+  uint8_t byteOne   = memory->byte[pc];
+  uint8_t byteTwo   = memory->byte[pc + 1];
+  uint8_t byteThree = memory->byte[pc + 2];
+  uint8_t byteFour  = memory->byte[pc + 3];
 
-	/* Checking memory valid access*/
-
-		uint8_t byteOne   = memory->byte[pc];
-		uint8_t byteTwo   = memory->byte[pc + 1];
-		uint8_t byteThree = memory->byte[pc + 2];
-		uint8_t byteFour  = memory->byte[pc + 3];
-		/* Do bit-wise shifting to concatenate the four bytes
-		 * Does automatic conversion from Little to Big Endian
-		 */
-		uint32_t result = byteOne | (byteTwo << 8) | (byteThree << 16) | (byteFour << 24);
-
-
-		return result;
+  //Do bit-wise shifting to concatenate the four bytes together
+  //Does automatic conversion from Little to Big Endian
+  return byteOne | (byteTwo << 8) | (byteThree << 16) | (byteFour << 24);
 }
 
 
+/**
+ * Writes the given word to a position in memory
+ */
 void
 memory_write_word(uint16_t memory_index, uint32_t word_to_write){
-	/* Does conversion from big to little endian for memory write */
-	uint32_t mask = 0x000000FF;
 
-//	if(memory->byte[memory_index] == NULL){
-//		printf("Error");
-//	}
-//	else{
+  //Does conversion from Big to Little endian for memory write
+  uint32_t mask = 0x000000FF;
 
-	    for(int i = 0; i < 4; i++){
-		    memory->byte[memory_index + i] = (word_to_write & mask) >> i * 8;
-		    mask <<= 8;
-	    }
-//	}
+  for(int i = 0; i < 4; i++){
+    memory->byte[memory_index + i] = (word_to_write & mask) >> i * 8;
+    mask <<= 8;
+  }
 }
+
 
 /**
  * Sets up machine memory
@@ -62,12 +64,10 @@ memory_write_word(uint16_t memory_index, uint32_t word_to_write){
 void
 memory_machine_init(memory_machine **memptr){
 
-	int numBytes = MEM_SIZE;
+  int numBytes = MEM_SIZE;
 	
-	/* 1 indicates that there is only 1 item in struct 
-	 * memory_machine -> may change in future && cause bugs*/
-	*memptr = calloc(1, sizeof(memory_machine)); 
-	(*memptr)->byte = calloc(numBytes, sizeof(uint8_t));
+  *memptr = calloc(1, sizeof(memory_machine)); 
+  (*memptr)->byte = calloc(numBytes, sizeof(uint8_t));
 }
 
 
@@ -76,8 +76,8 @@ memory_machine_init(memory_machine **memptr){
  * @param memptr Pointer to macine memory
  */
 void
-memory_machine_destroy(memory_machine *memptr){
-	free(memptr);
+memory_machine_destroy(memory_machine **memptr){
+  free(memptr);
 }
 
 
@@ -85,10 +85,8 @@ memory_machine_destroy(memory_machine *memptr){
  * Sets up CPU 
  */
 void
-memory_cpu_init(){
-	/* Need to implement with calloc to initialise to zero*/
-	/* source of error in calloc due to size of 1 as it can change */
-	cpu_ptr = calloc(1, sizeof(cpu));
+memory_cpu_init(void){
+  cpu_ptr = calloc(1, sizeof(cpu));
 }
 
 
@@ -96,26 +94,8 @@ memory_cpu_init(){
  * Frees CPU
  */
 void
-memory_cpu_destroy(){
-	free(cpu_ptr);
-}
-
-
-/* Initialsing memory for instruction struct */
-
-instr_data_proc_struct *instr_data_proc_ptr;
-instr_mult_struct *instr_mult_ptr;
-instr_single_data_trans_struct *instr_single_data_trans_ptr;
-instr_branch_struct *instr_branch_ptr;
-
-
-/**
- * Sets up instruction memory
- */
-void
-memory_instr_data_proc_init(){
-	/* source of error in calloc due to size of 1 as it can change */
-	instr_data_proc_ptr = calloc(1, sizeof(instr_data_proc_struct));
+memory_cpu_destroy(void){
+  free(cpu_ptr);
 }
 
 
@@ -123,9 +103,8 @@ memory_instr_data_proc_init(){
  * Sets up instruction memory
  */
 void
-memory_instr_mult_init(){
-	/* source of error in calloc due to size of 1 as it can change */
-	instr_mult_ptr = calloc(1, sizeof(instr_mult_struct));
+memory_instr_data_proc_init(void){
+  instr_data_proc_ptr = calloc(1, sizeof(instr_data_proc_struct));
 }
 
 
@@ -133,9 +112,8 @@ memory_instr_mult_init(){
  * Sets up instruction memory
  */
 void
-memory_instr_single_data_transfer_init(){
-	/* source of error in calloc due to size of 1 as it can change */
-	instr_single_data_trans_ptr = calloc(1, sizeof(instr_single_data_trans_struct));
+memory_instr_mult_init(void){
+  instr_mult_ptr = calloc(1, sizeof(instr_mult_struct));
 }
 
 
@@ -143,28 +121,17 @@ memory_instr_single_data_transfer_init(){
  * Sets up instruction memory
  */
 void
-memory_instr_branch_init(){
-	/* source of error in calloc due to size of 1 as it can change */
-	instr_branch_ptr = calloc(1, sizeof(instr_branch_struct));
-}
-
-
-
-/**
- * Frees instruction memory
- */
-void
-memory_instr_data_proc_destroy(){
-	free(instr_data_proc_ptr);
+memory_instr_single_data_transfer_init(void){
+  instr_single_data_trans_ptr = calloc(1, sizeof(instr_single_data_trans_struct));
 }
 
 
 /**
- * Frees instruction memory
+ * Sets up instruction memory
  */
 void
-memory_instr_mult_destroy(){
-	free(instr_mult_ptr);
+memory_instr_branch_init(void){
+  instr_branch_ptr = calloc(1, sizeof(instr_branch_struct));
 }
 
 
@@ -172,8 +139,8 @@ memory_instr_mult_destroy(){
  * Frees instruction memory
  */
 void
-memory_instr_single_data_transfer_destroy(){
-	free(instr_single_data_trans_ptr);
+memory_instr_data_proc_destroy(void){
+  free(instr_data_proc_ptr);
 }
 
 
@@ -181,37 +148,64 @@ memory_instr_single_data_transfer_destroy(){
  * Frees instruction memory
  */
 void
-memory_instr_branch_destroy(){
-	free(instr_branch_ptr);
+memory_instr_mult_destroy(void){
+  free(instr_mult_ptr);
 }
 
 
+/**
+ * Frees instruction memory
+ */
 void
-memory_instr_flags_init(){
-	/* source of error in calloc due to size of 1 as it can change */
-	instr_flags_ptr = calloc(1, sizeof(instr_flags));
-}
-
-void
-memory_instr_flags_destroy(){
-	free(instr_flags_ptr);
+memory_instr_single_data_transfer_destroy(void){
+  free(instr_single_data_trans_ptr);
 }
 
 
-#define UNDEFINED 0xFFFFFFFF
-
-
-
+/**
+ * Frees instruction memory
+ */
 void
-memory_pipeline_init(){
-	pipeline_ptr = calloc(1, sizeof(pipeline_obj));
-	pipeline_ptr->fetched = UNDEFINED;
-	pipeline_ptr->decoded = UNDEFINED;
+memory_instr_branch_destroy(void){
+  free(instr_branch_ptr);
 }
 
+
+/**
+ * Sets up flag struct
+ */
 void
-memoyr_pipeline_destroy(){
-	free(pipeline_ptr);
+memory_instr_flags_init(void){
+  instr_flags_ptr = calloc(1, sizeof(instr_flags));
+}
+
+
+/**
+ * Frees flag struct memory
+ */
+void
+memory_instr_flags_destroy(void){
+  free(instr_flags_ptr);
+}
+
+
+/**
+ * Sets up the pipeline
+ */
+void
+memory_pipeline_init(void){
+  pipeline_ptr = calloc(1, sizeof(pipeline_obj));
+  pipeline_ptr->fetched = UNDEFINED;
+  pipeline_ptr->decoded = UNDEFINED;
+}
+
+
+/*
+ * Frees the pipeline
+ */
+void
+memoyr_pipeline_destroy(void){
+  free(pipeline_ptr);
 }
 
 
@@ -221,13 +215,12 @@ memoyr_pipeline_destroy(){
  */
 void
 check_file_error(FILE *file){
-	if(feof(file)){
-		//TODO
-		//printf("Reached end of file\n");
-	}
-	else if(ferror(file)){
-		printf("File error\n");
-	}
+  if(feof(file)){
+
+  }
+  else if(ferror(file)){
+    printf("File error\n");
+  }
 }
 
 
@@ -238,39 +231,37 @@ check_file_error(FILE *file){
 void 
 memory_load_file(FILE *file){
 
-    memory = NULL;
-	memory_machine_init(&memory);
-	memory_instr_flags_init();
-	memory_cpu_init();
-	memory_instr_data_proc_init();
-	memory_instr_mult_init();
-	memory_instr_single_data_transfer_init();
-	memory_instr_branch_init();
-	memory_pipeline_init();
+  memory = NULL;
+  memory_machine_init(&memory);
+  memory_instr_flags_init();
+  memory_cpu_init();
+  memory_instr_data_proc_init();
+  memory_instr_mult_init();
+  memory_instr_single_data_transfer_init();
+  memory_instr_branch_init();
+  memory_pipeline_init();
 	
+  for(int i = 0; i < MEM_SIZE; i++){
+    if(fread(memory->byte + i, BYTES, 1, file) == 1){
+      continue;
+    }
+    else{
+      check_file_error(file);
+      break;
+    }
+  }
 
-	for(int i = 0; i < MEM_SIZE; i++){
-		if(fread(memory->byte + i, BYTES, 1, file) == 1){
-			//printf("Line 222 in mem_proc.c\n");
-			continue;
-		}
-		else{
-			check_file_error(file);
-			break;
-		}
+  cpu_cycle();
 
-	}
+  //Deallocate memory
 
-	cpu_cycle();
-
-	//Possible deallocation of memory here
-
-//	memory_machine_destroy(&memory);
-//	memory_cpu_destroy();
-//	memory_instr_data_proc_destroy();
-//	memory_instr_mult_destroy();
-//	memory_instr_single_data_transfer_destroy();
-//	memory_instr_branch_destroy();
-
-	fclose(file);
+  /*
+  memory_machine_destroy(&memory);
+  memory_cpu_destroy();
+  memory_instr_data_proc_destroy();
+  memory_instr_mult_destroy();
+  memory_instr_single_data_transfer_destroy();
+  memory_instr_branch_destroy();
+*/
+  fclose(file);
 }
--- src/memory_proc.h
+++ src/memory_proc.h
@@ -1,15 +1,14 @@
 #ifndef MEMORY_PROC_H
 #define MEMORY_PROC_H
 
-/* No need to include headers here -> they are included in the common_instances file*/
+
 #include "common_instances.h"
 
 
 #define MEM_SIZE 65536
-/* Declaring the variable */
+#define BYTES    1
+#define UNDEFINED 0xFFFFFFFF
 
-/* Possible error with this pointer TODO*/
-extern int numBytes;
 
 typedef struct{
 	uint8_t *byte; 
@@ -17,14 +16,31 @@ typedef struct{
 
 extern memory_machine *memory;
 
-/* Declare all function prototypes here */
+
+/* Function Prototypes */
+
+
 uint32_t memory_swap_word_be_to_le(uint32_t word);
 uint32_t memory_fetch_word(uint32_t pc);
 void memory_write_word(uint16_t memory_index, uint32_t word_to_write);
 void memory_machine_init(memory_machine **memptr);
-void memory_machine_destroy(memory_machine *memptr);
-void memory_cpu_init();
-void memory_cpu_destroy();
+void memory_machine_destroy(memory_machine **memptr);
+void memory_cpu_init(void);
+void memory_cpu_destroy(void);
+void memory_instr_data_proc_init(void);
+void memory_instr_mult_init(void);
+void memory_instr_single_data_transfer_init(void);
+void memory_instr_branch_init(void);
+void memory_instr_data_proc_destroy(void);
+void memory_instr_mult_destroy(void);
+void memory_instr_single_data_transfer_destroy(void);
+void memory_instr_branch_destroy(void);
+void memory_instr_flags_init(void);
+void memory_instr_flags_destroy(void);
+void memory_pipeline_init(void);
+void memory_pipeline_destroy(void);
+
+
 void check_file_error(FILE *file);
 void memory_load_file(FILE *file);
 
